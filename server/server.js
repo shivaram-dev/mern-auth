6f27275a45cfb60debd505e01a519806a923f1a2
Shiva ram <shivarambairu.2002@gmail.com>
2025-11-08 15:29:00 +0530

    fix: database connection error

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB();
+await connectDB();
 
 const allowedOrigins = [
   'http://localhost:5173',
